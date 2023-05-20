ec68d1d175252e3c237c072cb1a52f837fc190c1
Alexandros Adam <alexadam@csd.auth.gr>
2023-05-20 17:08:54 +0300

    progress in form

--- src/components/MutliStepForm/MultiStepForm.tsx
+++ src/components/MutliStepForm/MultiStepForm.tsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import { formWrapperContainer } from "./styles";
-import { Step, StepLabel, Stepper } from "@mui/material";
+import { Button, Step, StepLabel, Stepper } from "@mui/material";
 import MultiStepFormProvider from "@hooks/useFormContext";
 import { UserForm } from "./components";
+import { useNavigate } from "react-router-dom";
+import { URLS } from "@constants/urls";
+import { Controller, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 export type FormData = {
   firstName: string;
@@ -34,9 +39,18 @@ type FormWrapperProps = {
 
 const steps = ["User info", "Address info", "Account info"];
 
+const multiStepFormScema = yup.object().shape({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  age: yup.number().required(),
+});
+
 const MultiStepForm = ({ formType }: FormWrapperProps) => {
   const [currentStep, setCurrentStep] = useState(0);
   const [formData, setFormData] = useState<FormData>(DEFAULT_DATA);
+  const isFirstFormStep = currentStep === 0;
+  const isLastFormStep = currentStep === steps.length - 1;
+  const navigate = useNavigate();
 
   const updateMultiStepFormValues = (newValues: Partial<FormData>) => {
     setFormData((prevValues) => ({
@@ -45,20 +59,25 @@ const MultiStepForm = ({ formType }: FormWrapperProps) => {
     }));
   };
 
+  const hookForm = useForm<FormData>({
+    mode: "onChange",
+    resolver: yupResolver(multiStepFormScema),
+  });
+
   const handleNextFormStep = () => setCurrentStep((activeStep) => activeStep + 1);
   const handlePreviousFormStep = () => setCurrentStep((activeStep) => activeStep - 1);
   const handleChangeFormStep = (index: number) => setCurrentStep(index);
+  const handleCancelFormStep = () => navigate(URLS.dashboard);
+
+  const onSubmit = () => {
+    console.log("Inside onSubmit");
+    handleNextFormStep();
+  };
 
   const getStepContent = (step: number): JSX.Element => {
     switch (step) {
       case 0:
-        return (
-          <UserForm
-            data={formData}
-            handleNextFormStep={handleNextFormStep}
-            updateMultiStepFormValues={updateMultiStepFormValues}
-          />
-        );
+        return <UserForm data={formData} />;
       case 1:
         return <div> Multi step 2</div>;
       case 2:
@@ -83,8 +102,23 @@ const MultiStepForm = ({ formType }: FormWrapperProps) => {
         ))}
       </Stepper>
       <h2 className="form_title">{steps[currentStep]}</h2>
-      <form>{getStepContent(currentStep)}</form>
-      {/* create buttons as well */}
+      <form onSubmit={hookForm.handleSubmit(onSubmit)}>
+        {getStepContent(currentStep)}
+        {/* create buttons as well */}
+        <div className="form-action-buttons">
+          <Button variant="contained" sx={{ mt: 3, mb: 2 }} onClick={handleCancelFormStep}>
+            Cancel
+          </Button>
+          {!isFirstFormStep && (
+            <Button variant="contained" sx={{ mt: 3, mb: 2 }} onClick={handlePreviousFormStep}>
+              Back
+            </Button>
+          )}
+          <Button variant="contained" sx={{ mt: 3, mb: 2 }}>
+            {isLastFormStep ? "Save" : "Next"}
+          </Button>
+        </div>
+      </form>
     </section>
   );
 };
--- src/components/MutliStepForm/components/UserForm.tsx
+++ src/components/MutliStepForm/components/UserForm.tsx
@@ -1,16 +1,5 @@
-import FormWrapper from "@components/MutliStepForm/MultiStepForm";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { FormContext } from "@hooks/useFormContext";
-import { Button, TextField } from "@mui/material";
-import { ChangeEvent, useContext } from "react";
-import { Controller, useForm } from "react-hook-form";
-import * as yup from "yup";
-
-const userFormScema = yup.object().shape({
-  firstName: yup.string().required(),
-  lastName: yup.string().required(),
-  age: yup.number().required(),
-});
+import { TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
 
 type UserData = {
   firstName: string;
@@ -20,50 +9,26 @@ type UserData = {
 
 type UserFormProps = {
   data: UserData;
-  handleNextFormStep: () => void;
-  updateMultiStepFormValues: (newValues: UserData) => void;
 };
 
-const UserForm = ({ data, handleNextFormStep, updateMultiStepFormValues }: UserFormProps) => {
-  const formContext = useContext(FormContext);
-  const { formState, register, handleSubmit, control } = useForm<UserData>({
+const UserForm = ({ data }: UserFormProps) => {
+  const { formState, register } = useForm<UserData>({
     mode: "onChange",
-    // resolver: yupResolver(userFormScema),
   });
   const { errors } = formState;
 
-  const onSubmit = (newValues: UserData, event: React.FormEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    console.log("lalala");
-    console.log("newValues = ", newValues);
-    // step 1: check error validation
-    // step 2: update form data using context
-    updateMultiStepFormValues(newValues);
-    // step 3: go on next step
-    handleNextFormStep();
-  };
-
-  const handleSubmitClick = () => alert("Clicked");
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Controller
-        name="firstName"
-        control={control}
-        render={({ field: onChange }) => (
-          <TextField
-            id="firstName"
-            label="First name"
-            value={data.firstName}
-            autoComplete="firstName"
-            margin="normal"
-            onChange={(event: ChangeEvent<HTMLInputElement>) => onChange(event.target.value)}
-            fullWidth
-            autoFocus
-            error={Boolean(errors.firstName)}
-            helperText={Boolean(errors.firstName) && "First name is a required field"}
-          />
-        )}
+    <>
+      <TextField
+        id="firstName"
+        label="First name"
+        value={data.firstName}
+        autoComplete="firstName"
+        margin="normal"
+        fullWidth
+        autoFocus
+        error={Boolean(errors.firstName)}
+        helperText={Boolean(errors.firstName) && "First name is a required field"}
       />
 
       <TextField
@@ -89,11 +54,7 @@ const UserForm = ({ data, handleNextFormStep, updateMultiStepFormValues }: UserF
         error={Boolean(errors.age)}
         helperText={Boolean(errors.age) && "Age is a required field"}
       />
-
-      <Button variant="contained" sx={{ mt: 3, mb: 2 }} type="submit">
-        Next
-      </Button>
-    </form>
+    </>
   );
 };
 
